6753527f1df8ee331dc593da65f4e6c716ffc47d
JBW <whitco670@jbw-mbp-215.attlocal.net>
2017-12-17 07:20:41 -0500

    Recevied some comments/help from Erick.  Spotify started parsing better

--- liri.js
+++ liri.js
@@ -2,48 +2,64 @@ var request = require("request");
 var nodeArgs = process.argv;
 let action = process.argv[2];
 var Twitter = require('twitter');
+let value = process.argv[3];
+var Spotify = require("node-spotify-api");
 
-
-
-
-
-//var screen_name = jBradw;
-var myTweets = function (){
-
- 
-var client = new Twitter({
-  consumer_key: 'iQeQtZRBKxHNql7CDbYy8kpYM',
-  consumer_secret: 'p8nge3Um8CqUS35TVnVPkKTpH34MdszqhLOpN6jm5kp76t3ZPO',
-  access_token_key: '941384945162997761-GPIUJva36RBxn53lGd3sEejU4dCjGUn',
-  access_token_secret: 'InYO5XrezQzrrWMibunM47wpuC1seIIwglqGSVNeJMNGS'
+////JSON.parse(body).Released
+//
+//
+var spotify = new Spotify({
+ id: "21e32b9b23b044ada953c89c4409b4cd",
+ secret: "46433a5a9e4747d489e9e1f3bc13f778"
 });
- 
-var params = {screen_name: '@jBradw1'};
-client.get('statuses/user_timeline', params, function(error, tweets, response) {
-  // console.log(' look params',params)
-  if (!error) {
-
-    for (var i = 0; i < tweets.length; i++) {
-      console.log(tweets[i].text)
-    }
-  }
-  else {console.log("this error", error);}
-});
-
-};
-myTweets();
+// 
+spotify.search({ type: 'track', query: "eli's coming", limit: 1 }, function(err, data) {
+ if (err) {
+   return console.log('Error occurred: ' + err);
+ }
+	console.log(data.tracks.items[0].album.name);
+	//console.log(JSON.parse(data));
+	//console.log (JSON.stringify(data)); 
+	//console.log(JSON.stringify(data).tracks);
+	//console.log("This is my test" + JSON.parse(choose).tracks);	
+})
+//
+//
 
 
+var myTweets = function() {
+	var client = new Twitter({
+	  consumer_key: 'iQeQtZRBKxHNql7CDbYy8kpYM',
+	  consumer_secret: 'p8nge3Um8CqUS35TVnVPkKTpH34MdszqhLOpN6jm5kp76t3ZPO',
+	  access_token_key: '941384945162997761-GPIUJva36RBxn53lGd3sEejU4dCjGUn',
+	  access_token_secret: 'InYO5XrezQzrrWMibunM47wpuC1seIIwglqGSVNeJMNGS'
+	});
+	var params = {screen_name: '@jBradw1'};
+	client.get('statuses/user_timeline', params, function(error, tweets, response) {
+	  // console.log(' look params',params)
+	  if (!error) {
+	    for (var i = 0; i < tweets.length; i++) {
+	      console.log(tweets[i].text)
+	    }
+	  }
+	  else {console.log("this error", error);}
+	});
+  };
+//myTweets();
 
 
 
 
 
 
-if (action === "movie-this"){
+
+
+
 	//movie();  for some reason i cant run this in a function. I need to follow-up with instructors
 
 //movie = ()=> {
+
+var movie = function(){
 
  movieName = "";
 	for (var i = 3; i < nodeArgs.length; i++) {
@@ -53,7 +69,7 @@ if (action === "movie-this"){
 	else {
 	movieName += nodeArgs[i];
 	}
-}
+	}
 	request("http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy", function(error, response, body) {
 		if (!error && response.statusCode === 200) {
 
@@ -66,10 +82,18 @@ if (action === "movie-this"){
 	console.log("The actors in the movie are " + JSON.parse(body).Actors)
 		}
 	});
-//	}
+	}
+	if (action === "movie-this"){
+		movie();
 	}
 	else if (action === "my-tweets"){
 		myTweets();
 	}
 
 	//movie();
+
+
+
+
+
+
